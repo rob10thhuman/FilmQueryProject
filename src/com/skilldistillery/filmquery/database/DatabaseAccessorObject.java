875a7363eaec4d1857945352993228a7d4ec1df3
rob10thHuman <rob@10thHuman.com>
2018-10-05 15:39:32 -0600

    initial commit

---
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -0,0 +1,24 @@
+package com.skilldistillery.filmquery.database;
+
+import java.util.List;
+
+public class DatabaseAccessorObject implements DatabaseAccessor {
+
+  @Override
+  public Film getFilmById(int filmId) {
+    return null;
+  }
+
+@Override
+public Actor getActorById(int actorId) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public List<Actor> getActorsByFilmId(int filmId) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+}
